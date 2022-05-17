a0caef22d2abd35b86e3e198351ad08e973fc7c9
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-17 22:45:25 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -15,7 +15,7 @@ module.exports = (server) => {
     const addNewUser = (username, socketId) => {
         !onlineUsers.some((user) => user.username === username) &&
             onlineUsers.push({ username, socketId });
-        console.log(11, socketId)
+        // console.log(11, socketId)
         console.log(22, onlineUsers)
     };
 
