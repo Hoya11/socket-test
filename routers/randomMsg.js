0c40e92c10b1407ff6aa35d33fdf8d23a6faafea
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-21 03:08:13 +0900

    ssa

--- routers/randomMsg.js
+++ routers/randomMsg.js
@@ -12,4 +12,9 @@ router.post('/', postRandomMsg);
 // 랜덤메시지 삭제(내부용)
 router.delete('/:randomMsgId', deleteRandomMsg);
 
+// 랜덤 프로필이미지 생성(내부용)
+router.post("/randomImg", postRandomImg)
+
+
+
 module.exports = router;
