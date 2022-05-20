8fc2115025d0c053bcc3d3ca72ed046057351b00
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-21 01:03:27 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -68,7 +68,7 @@ module.exports = (server) => {
         }));
 
         socket.on("join", (async (userId) => {
-            const findRoom = await Room.find(familyMemberList.userId)
+            const findRoom = await Room.find({ familyMemberList: familyMemberList.userId })
             console.log(222, findRoom)
             const findRoomId = findRoom.roomId
             socket.join(findRoomId)
