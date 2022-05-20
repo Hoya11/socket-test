2b06aa2082369534ef12649c0bf5480c172acc19
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-20 23:09:04 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -66,7 +66,7 @@ module.exports = (server) => {
             console.log(userId)
 
             const findRoom = await Room.findOne({ userId })
-            const findRoomId = findRoom._id
+            const findRoomId = findRoom._id.split('"')
             console.log(findRoomId)
 
             socket.join(roomName)
