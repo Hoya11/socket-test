593759c08febcddc36bdc12c7b6ee922a176158e
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-22 20:21:48 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -54,6 +54,8 @@ module.exports = server => {
         return onlineUsers.find(user => user.userId === userId)
     }
 
+    console.log("onlineUsers =>", onlineUsers)
+
     //* 웹소켓 연결 시
     io.on("connection", socket => {
         console.log("소켓 연결됨", socket.id)
